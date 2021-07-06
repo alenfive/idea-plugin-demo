d8440facb80e41829c42204423d48746caedb552
GitHub Action <action@github.com>
2021-07-06 02:04:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/alenfive/ideaplugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/alenfive/ideaplugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.alenfive.ideaplugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alenfive.ideaplugindemo.MyBundle
 
 class MyApplicationService {
 
